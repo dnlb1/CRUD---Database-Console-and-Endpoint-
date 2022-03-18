0865f51b4ffdbc531006a0e4f9ce545a64f5ce22
Bencze Dániel <rgrengarhyped@gmail.com>
2022-03-18 10:57:38 +0100

    Add project files.

---
+++ UHPYQ8_HFT_2021222.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UHPYQ8_HFT_2021222.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
