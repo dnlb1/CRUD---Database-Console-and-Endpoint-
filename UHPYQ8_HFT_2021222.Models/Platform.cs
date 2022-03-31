87ce252726a9bfcdc5f4428b6ff6ae421bd59199
Bencze Dániel <rgrengarhyped@gmail.com>
2022-03-31 16:09:16 +0200

    Platform cs filled with properties/ctors

--- UHPYQ8_HFT_2021222.Models/Platform.cs
+++ UHPYQ8_HFT_2021222.Models/Platform.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,24 @@ namespace UHPYQ8_HFT_2021222.Models
 {
     public class Platform
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PlatformId { get; set; }
+        public string PlatformName { get; set; }
+
+        public virtual ICollection<Game> Games { get; set; }
+
+        public Platform()
+        {
+            Games = new HashSet<Game>();
+        }
+
+        public Platform(string line)
+        {
+            string[] split = line.Split('#');
+            PlatformId = int.Parse(split[0]);
+            PlatformName = split[1];
+            Games = new HashSet<Game>();
+        }
     }
 }
