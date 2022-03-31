e8a2e15dfa339ea1ac0dc3281e7939891455bfec
Bencze Dániel <rgrengarhyped@gmail.com>
2022-03-31 16:17:40 +0200

    Tables filled up with datas

--- UHPYQ8_HFT_2021222.Repository/GameDbContext.cs
+++ UHPYQ8_HFT_2021222.Repository/GameDbContext.cs
@@ -38,6 +38,45 @@ namespace UHPYQ8_HFT_2021222.Repository
             .HasForeignKey(Game => Game.PlatformId)
             .OnDelete(DeleteBehavior.Cascade));
 
+            modelBuilder.Entity<Game>().HasData(new Game[]
+         {
+                new Game("1#Elden Ring#50#1#7,9#1"),
+                new Game("2#Black Desert Online#30#2#8,7#1"),
+                new Game("3#League of Legends#19#1#9,2#1"),
+                new Game("4#Mario Kart#20#3#5,4#2"),
+                new Game("5#Destiny 2#40#4#7,1#2"),
+                new Game("6#God of War#77#5#9,2#3"),
+                new Game("7#Roblox#10#6#3,3#4"),
+                new Game("8#Minecraft#59#7#8,6#5"),
+                new Game("9#Fortnite#23#8#4,6#6"),
+                new Game("10#Hades#30#9#2,2#6"),
+
+         });
+
+            modelBuilder.Entity<Publisher>().HasData(new Publisher[]
+          {
+                   new Publisher("1#Kakao Games"),
+                   new Publisher("2#Sajobabonyi Zrt."),
+                   new Publisher("3#Tencent Games"),
+                   new Publisher("4#Sony Interactive Entertainment"),
+                   new Publisher("5#Nintendo"),
+                   new Publisher("6#Microsoft"),
+                   new Publisher("7#Java"),
+                   new Publisher("8#Pankix"),
+                   new Publisher("9#Ubisoft"),
+                   new Publisher("10#Capcorn")
+
+          });
+            modelBuilder.Entity<Platform>().HasData(new Platform[]
+          {
+                new Platform("1#Steam"),
+                new Platform("2#Epic Launcher"),
+                new Platform("3#XBOX"),
+                new Platform("4#Switch"),
+                new Platform("5#Mobile"),
+                new Platform("6#IOS"),
+          });
+
         }
     }
 }
