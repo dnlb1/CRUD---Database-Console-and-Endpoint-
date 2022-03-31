0799d53888d245a6d3f8c03202b76f031530515f
Bencze Dániel <rgrengarhyped@gmail.com>
2022-03-31 18:28:54 +0200

    Game_Publisher filled up with properties/ctors

--- UHPYQ8_HFT_2021222.Models/Game_publisher.cs
+++ UHPYQ8_HFT_2021222.Models/Game_publisher.cs
@@ -8,5 +8,29 @@ namespace UHPYQ8_HFT_2021222.Models
 {
     public class Game_publisher
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int GP_Id { get; set; }
+        public int release_year { get; set; }
+
+        public int GameId { get; set; }
+        public int PublisherId { get; set; }
+
+        public virtual Game Game { get; private set; }
+        public virtual Publisher Publisher { get; private set; }
+
+        public Game_publisher()
+        {
+
+        }
+
+        public Game_publisher(string line)
+        {
+            string[] split = line.Split('#');
+            GP_Id = int.Parse(split[0]);
+            GameId = int.Parse(split[1]);
+            PublisherId = int.Parse(split[2]);
+            release_year = int.Parse(split[3]);
+        }
     }
 }
