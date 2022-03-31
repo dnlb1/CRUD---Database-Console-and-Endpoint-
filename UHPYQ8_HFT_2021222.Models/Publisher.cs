37e5c59c4d4ad820195c1e3182c382e69c3546d1
Bencze Dániel <rgrengarhyped@gmail.com>
2022-03-31 16:10:35 +0200

    Publisher cs filled with properties/ctors

--- UHPYQ8_HFT_2021222.Models/Publisher.cs
+++ UHPYQ8_HFT_2021222.Models/Publisher.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,27 @@ namespace UHPYQ8_HFT_2021222.Models
 {
     public class Publisher
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PublisherId { get; set; }
+
+        [Required]
+        [StringLength(240)]
+        public string PublisherName { get; set; }
+
+        public virtual ICollection<Game> Games { get; set; }
+
+        public Publisher()
+        {
+            Games = new HashSet<Game>();
+        }
+
+        public Publisher(string line)
+        {
+            string[] split = line.Split('#');
+            PublisherId = int.Parse(split[0]);
+            PublisherName = split[1];
+            Games = new HashSet<Game>();
+        }
     }
 }
