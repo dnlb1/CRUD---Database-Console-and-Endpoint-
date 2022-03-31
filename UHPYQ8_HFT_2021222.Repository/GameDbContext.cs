010dd2a2359fa0e15afab1b4f67fe9880ce9dfa6
Bencze Dániel <rgrengarhyped@gmail.com>
2022-03-31 16:15:34 +0200

    3 Tables created/Ensured method in ctor

--- UHPYQ8_HFT_2021222.Repository/GameDbContext.cs
+++ UHPYQ8_HFT_2021222.Repository/GameDbContext.cs
@@ -1,8 +1,20 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using UHPYQ8_HFT_2021222.Models;
 
 namespace UHPYQ8_HFT_2021222.Repository
 {
-    public class GameDbContext 
+    public class GameDbContext : DbContext
     {
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Platform> Platforms { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+
+        public GameDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+
     }
 }
