35ce195668f7e92ea9ed23d44fe86261a9ed21e4
Bencze Dániel <rgrengarhyped@gmail.com>
2022-04-12 15:15:59 +0200

    Generic Repository created

---
+++ UHPYQ8_HFT_2021222.Repository/GenericRepository/Repository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UHPYQ8_HFT_2021222.Repository.Interface;
+
+namespace UHPYQ8_HFT_2021222.Repository.GenericRepository
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+    }
+}
