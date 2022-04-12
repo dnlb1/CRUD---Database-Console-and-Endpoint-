decc116e5485bf18264c861fa6e1aa62423682fb
Bencze Dániel <rgrengarhyped@gmail.com>
2022-04-12 15:26:10 +0200

    Game_PublisherRepository created

---
+++ UHPYQ8_HFT_2021222.Repository/ModelRepositories/Game_publisherRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UHPYQ8_HFT_2021222.Models;
+using UHPYQ8_HFT_2021222.Repository.GenericRepository;
+using UHPYQ8_HFT_2021222.Repository.Interface;
+
+namespace UHPYQ8_HFT_2021222.Repository.ModelRepositories
+{
+    public class Game_publisherRepository : Repository<Game_publisher>, IRepository<Game_publisher>
+    {
+    }
+}
