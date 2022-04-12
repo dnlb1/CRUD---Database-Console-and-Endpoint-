57300ed71fd9db8032cf313d6e4f83d6b0f0ff92
Bencze Dániel <rgrengarhyped@gmail.com>
2022-04-12 15:13:44 +0200

    CRUD + ReadAll metods added to the interface

--- UHPYQ8_HFT_2021222.Repository/Interface/IRepository.cs
+++ UHPYQ8_HFT_2021222.Repository/Interface/IRepository.cs
@@ -8,5 +8,10 @@ namespace UHPYQ8_HFT_2021222.Repository.Interface
 {
     public interface IRepository<T> where T : class
     {
+        void Create(T item);
+        T Read(int id);
+        void Update(T item);
+        void Delete(int id);
+        IQueryable<T> ReadAll();
     }
 }
