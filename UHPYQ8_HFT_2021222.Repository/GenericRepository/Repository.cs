4d395d473f642eea23591f7935068fbbb5af5026
Bencze Dániel <rgrengarhyped@gmail.com>
2022-04-12 15:16:17 +0200

    Implemented the CRUD methods

--- UHPYQ8_HFT_2021222.Repository/GenericRepository/Repository.cs
+++ UHPYQ8_HFT_2021222.Repository/GenericRepository/Repository.cs
@@ -9,5 +9,29 @@ namespace UHPYQ8_HFT_2021222.Repository.GenericRepository
 {
     public abstract class Repository<T> : IRepository<T> where T : class
     {
+        public void Create(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
