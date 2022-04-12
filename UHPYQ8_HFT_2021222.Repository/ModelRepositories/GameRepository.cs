b76a53df8d041ba5cd898d037783903f5bd998fd
Bencze Dániel <rgrengarhyped@gmail.com>
2022-04-12 15:20:53 +0200

    GameRepository created

---
+++ UHPYQ8_HFT_2021222.Repository/ModelRepositories/GameRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UHPYQ8_HFT_2021222.Models;
+using UHPYQ8_HFT_2021222.Repository.GenericRepository;
+using UHPYQ8_HFT_2021222.Repository.Interface;
+
+namespace UHPYQ8_HFT_2021222.Repository.ModelRepositories
+{
+    public class GameRepository : Repository<Game>, IRepository<Game>
+    {
+
+    }
+}
