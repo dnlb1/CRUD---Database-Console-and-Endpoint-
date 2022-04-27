528e002130418f50cf5446a182c68e5ad5f4a36c
Bencze Dániel <rgrengarhyped@gmail.com>
2022-04-27 18:38:45 +0200

    Statcontroller created

---
+++ UHPYQ8_HFT_2021222.Endpoint/Controllers/StatController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UHPYQ8_HFT_2021222.Logic.Classes;
+using UHPYQ8_HFT_2021222.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UHPYQ8_HFT_2021222.Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class StatController : ControllerBase
+    {
+        IGameLogic gameLogic;
+
+        public StatController(IGameLogic gameLogic)
+        {
+            this.gameLogic = gameLogic;
+        }
+
+        [HttpGet]
+        public double GameAVGPrice()
+        {
+            return gameLogic.GameAVGPrice();
+        }
+
+        [HttpGet]
+        public double GameAVGRating()
+        {
+            return gameLogic.GameAVGRating();
+        }
+
+        [HttpGet("{Platform}")]
+        public IEnumerable<Game> FindAllGameAtThisPlatform(string Platform)
+        {
+            return gameLogic.FindAllGameAtThisPlatform(Platform);
+        }
+
+        [HttpGet]
+        public IEnumerable<KeyValuePair<string, double>> PlatformICMoney()
+        {
+            return gameLogic.PlatformICMoney();
+        }
+
+        [HttpGet]
+        public IEnumerable<KeyValuePair<string, double>> GameCountByPlatform()
+        {
+            return gameLogic.GameCountByPlatform();
+        }
+        [HttpGet]
+        public IEnumerable<KeyValuePair<string, double>> AVGRatingByPlatform()
+        {
+            return gameLogic.AVGRatingByPlatform();
+        }
+
+        [HttpGet]
+        public IEnumerable<KeyValuePair<string, double>> AVGPriceByPlatform()
+        {
+            return gameLogic.AVGPriceByPlatform();
+        }
+    }
+}
