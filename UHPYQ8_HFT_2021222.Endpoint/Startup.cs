b33eb7d567b310215009b0b09e744bfe5863ad5a
Bencze Dániel <rgrengarhyped@gmail.com>
2022-04-27 18:17:05 +0200

    Startup page is ready - using exceptionhandler too

--- UHPYQ8_HFT_2021222.Endpoint/Startup.cs
+++ UHPYQ8_HFT_2021222.Endpoint/Startup.cs
@@ -1,21 +1,54 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UHPYQ8_HFT_2021222.Logic.Classes;
+using UHPYQ8_HFT_2021222.Models;
+using UHPYQ8_HFT_2021222.Repository;
+using UHPYQ8_HFT_2021222.Repository.Interface;
+using UHPYQ8_HFT_2021222.Repository.ModelRepositories;
 
-namespace UHPYQ8_HFT_2021222.Endpoint
+namespace Feleves.Endpoint2
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<GameDbContext>();
+
+            services.AddTransient<IRepository<Game>, GameRepository>();
+            services.AddTransient<IRepository<Game_publisher>, Game_publisherRepository>();
+            services.AddTransient<IRepository<Platform>, PlatformRepository>();
+            services.AddTransient<IRepository<Publisher>, PublisherRepository>();
+
+            services.AddTransient<IGameLogic, GameLogic>();
+            services.AddTransient<IGame_publisherLogic, Game_publisherLogic>();
+            services.AddTransient<IPlatformLogic, PlatformLogic>();
+            services.AddTransient<IPublisherLogic, PublisherLogic>();
+
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Feleves.Endpoint2", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,16 +57,24 @@ namespace UHPYQ8_HFT_2021222.Endpoint
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Feleves.Endpoint2 v1"));
             }
+
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var exception = context.Features.Get<IExceptionHandlerPathFeature>().Error;
+                var response = new { Msg = exception.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
