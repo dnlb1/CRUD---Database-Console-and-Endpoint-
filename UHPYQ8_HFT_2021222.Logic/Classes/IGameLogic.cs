40a10846f400d02c98f10b6c7495b3b90c94a2a3
Bencze Dániel <rgrengarhyped@gmail.com>
2022-04-27 17:36:07 +0200

    IGameLogic Interface created

---
+++ UHPYQ8_HFT_2021222.Logic/Classes/IGameLogic.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using UHPYQ8_HFT_2021222.Models;
+
+namespace UHPYQ8_HFT_2021222.Logic.Classes
+{
+    public interface IGameLogic
+    {
+        void Create(Game item);
+        void Delete(int id);
+        Game Read(int id);
+        IQueryable<Game> ReadAll();
+        void Update(Game item);
+    }
+}
