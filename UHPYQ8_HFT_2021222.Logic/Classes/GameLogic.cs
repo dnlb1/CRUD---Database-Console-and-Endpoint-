5192e579e92a0c7a980b94eb9cabfb262c5850f4
Bencze Dániel <rgrengarhyped@gmail.com>
2022-04-27 17:35:43 +0200

    GameLogic CRUD methods created

--- UHPYQ8_HFT_2021222.Logic/Classes/GameLogic.cs
+++ UHPYQ8_HFT_2021222.Logic/Classes/GameLogic.cs
@@ -3,10 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UHPYQ8_HFT_2021222.Models;
+using UHPYQ8_HFT_2021222.Repository.Interface;
 
 namespace UHPYQ8_HFT_2021222.Logic.Classes
 {
     public class GameLogic
     {
+        IRepository<Game> repo;
+
+        public GameLogic(IRepository<Game> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Game item)
+        {
+            if (item.Title.Length < 3)
+            {
+                throw new ArgumentException("Not enough long title.");
+            }
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Game Read(int id)
+        {
+            var game = this.repo.Read(id);
+            if (game == null)
+            {
+                throw new ArgumentException("This game not exists");
+            }
+            return game;
+        }
+
+        public IQueryable<Game> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Game item)
+        {
+            this.repo.Update(item);
+        }
     }
 }
