1e7eda4c229d21a19860527ae7d07560bcf3632c
Bencze Dániel <rgrengarhyped@gmail.com>
2022-04-27 17:38:52 +0200

    IGameLogic interface changed

--- UHPYQ8_HFT_2021222.Logic/Classes/IGameLogic.cs
+++ UHPYQ8_HFT_2021222.Logic/Classes/IGameLogic.cs
@@ -1,12 +1,20 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UHPYQ8_HFT_2021222.Models;
 
 namespace UHPYQ8_HFT_2021222.Logic.Classes
 {
     public interface IGameLogic
     {
+        IEnumerable<KeyValuePair<string, double>> AVGPriceByPlatform();
+        IEnumerable<KeyValuePair<string, double>> AVGRatingByPlatform();
         void Create(Game item);
         void Delete(int id);
+        IEnumerable<Game> FindAllGameAtThisPlatform(string PlatformName);
+        double GameAVGPrice();
+        double GameAVGRating();
+        IEnumerable<KeyValuePair<string, double>> GameCountByPlatform();
+        IEnumerable<KeyValuePair<string, double>> PlatformICMoney();
         Game Read(int id);
         IQueryable<Game> ReadAll();
         void Update(Game item);
