c3175f4b1da25c01aa70d1e99c81b9975572e4fe
Bencze Dániel <rgrengarhyped@gmail.com>
2022-04-27 17:40:14 +0200

    IPlatformLogic created

---
+++ UHPYQ8_HFT_2021222.Logic/Classes/IPlatformLogic.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using UHPYQ8_HFT_2021222.Models;
+
+namespace UHPYQ8_HFT_2021222.Logic.Classes
+{
+    public interface IPlatformLogic
+    {
+        void Create(Platform item);
+        void Delete(int id);
+        Platform Read(int id);
+        IQueryable<Platform> ReadAll();
+        void Update(Platform item);
+    }
+}
