e08701a0ddbeff3fd67110bb0ef8607cc5ab3c19
Bencze Dániel <rgrengarhyped@gmail.com>
2022-04-27 17:41:15 +0200

    PublisherLogic created-filled with crud methods

---
+++ UHPYQ8_HFT_2021222.Logic/Classes/PublisherLogic.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UHPYQ8_HFT_2021222.Models;
+using UHPYQ8_HFT_2021222.Repository.Interface;
+
+namespace UHPYQ8_HFT_2021222.Logic.Classes
+{
+    public class PublisherLogic
+    {
+        IRepository<Publisher> repo;
+
+        public PublisherLogic(IRepository<Publisher> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Publisher item)
+        {
+            if (item.PublisherName.Length < 1)
+            {
+                throw new ArgumentException("Not enough long Publisher name.");
+            }
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Publisher Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Publisher> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Publisher item)
+        {
+            this.repo.Update(item);
+        }
+    }
+}
