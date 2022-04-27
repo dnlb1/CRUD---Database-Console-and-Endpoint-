6b7ad4f50ca0c376e29a5c40a96bfc7a7bf3b26e
Bencze Dániel <rgrengarhyped@gmail.com>
2022-04-27 18:23:51 +0200

    Title Changed

--- UHPYQ8_HFT_2021222.Endpoint/Startup.cs
+++ UHPYQ8_HFT_2021222.Endpoint/Startup.cs
@@ -47,18 +47,17 @@ namespace UHPYQ8_HFT_2021222.Endpoint
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Feleves.Endpoint2", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "UHPYQ8_HFT_2021222.Endpoint", Version = "v1" });
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Feleves.Endpoint2 v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "UHPYQ8_HFT_2021222.Endpoint v1"));
             }
 
             app.UseExceptionHandler(c => c.Run(async context =>
